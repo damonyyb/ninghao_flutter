bf3de0f8daff69cae46b7a9e87798aff0ea218f5
叶一波 <961355618@qq.com>
2020-02-06 23:18:57 +0800

    first finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
+import 'demo/listview_demo.dart';
+import 'demo/draw_demo.dart';
+import 'demo/bottom_navigation_bar_demo.dart';
 
 void main() => runApp(App());
 
@@ -8,67 +10,56 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Home(),
-      theme: ThemeData(
-        primarySwatch:Colors.yellow
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+        theme: ThemeData(
+            primarySwatch: Colors.yellow,
+            highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+            splashColor: Colors.white70));
   }
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-      color:Colors.white,
-      margin:EdgeInsets.all(8.0),
-      child:Column (
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-            posts[index].title,
-            style:Theme.of(context).textTheme.title
-          ),
-          Text(
-            posts[index].author,
-            style:Theme.of(context).textTheme.subhead
-          ),
-          SizedBox(height: 16.0),
-        ],
-      )
-    );
-  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          title:Text('NingHao'),
-          elevation: 0.0,
-        ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _listItemBuilder,
-        ),
-      );
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            title: Text('NingHao'),
+            elevation: 0.0,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: () => debugPrint('Search button is Pressed.'),
+              )
+            ],
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              ListViewDemo(),
+              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            ],
+          ),
+          drawer: DrawDemo(),
+          bottomNavigationBar: BottomNavigationBarDemo(),
+        ));
   }
 }
 
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child:Text(
-        'hello',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 40.0,
-          fontWeight:FontWeight.bold,
-          color:Colors.black87,
-        )
-      ),
-    );
-  }
-}
